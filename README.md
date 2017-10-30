2a772665d6a17b73ced978858c7a5eaa5231e97c
Mike Dane <giraffeacademy@gmail.com>
2017-10-30 11:22:30 -0400

    Added v1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
+# DentistSmile - Jekyll Theme
+
+A single page Jekyll theme for dental clinics. It's easily customizable and responsive. It comes with svg icons, google map and a working contact form. Docs can be found [here](http://obaez.com/dentistsmile-docs/).
+
+Here is a [live Demo](http://obaez.com/dentistsmile/)
+
+=======
 # kpehotwings.com
+>>>>>>> 713d90af96bfccb4a60745173688317ad4c4082d
---
+++ build_site.sh
@@ -0,0 +1,38 @@
+# ----- U S A G E -----
+# Call this script from the root directory of the project
+# _helpers/shell-scripts/build_site.sh "git commit message"
+# ---------------------
+
+git config credential.username "mikedane"
+
+echo "Deleting old publication"
+rm -rf _site
+mkdir _site
+git worktree prune
+rm -rf .git/worktrees/_site/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages _site origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+jekyll build
+
+echo "Updating master branch"
+git add .
+git commit -m "$1"
+git push origin master
+
+echo "Updating gh-pages branch"
+cd _site && git add --all && git commit -m "$1"
+git push origin gh-pages
+cd ..
+
+# echo "Updating code samples"
+# cd code-samples
+# git add .
+# git commit -m "let's learn"
+# git push origin master
+# cd ..
